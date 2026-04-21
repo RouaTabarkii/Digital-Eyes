11c95c96babd9e1c4960e8f9452dcfccc49cc139
RouaTabarkii <tbarkirouee@gmail.com>
2026-04-21 22:47:29 +0100

    fastAPI install

--- Backend/main.py
+++ Backend/main.py
@@ -9,3 +9,7 @@ def root() :
 @app.post("/DigitalEyes")
 def root() :
     return {"message": "Hello World"}
+
+@app.get("/DigitalEyes/{id}")
+def root(id: int) :
+    return {"message": f"Hello World {id}"}
